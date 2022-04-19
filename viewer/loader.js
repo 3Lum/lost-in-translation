c7b7e55a7f5e262e282b1e482177cdea8fbedbf3
elum <psdwyw@gmail.com>
2022-04-19 13:32:48 -0700

    e

--- viewer/loader.js
+++ viewer/loader.js
@@ -1,6 +1,6 @@
-import * as THREE from './js/three.module.js';
-import { OrbitControls } from './js/OrbitControls.js';
-import {GLTFLoader} from './js/GLTFLoader.js';
+import * as THREE from './viewer/js/three.module.js';
+import { OrbitControls } from './viewer/js/OrbitControls.js';
+import {GLTFLoader} from './viewer/js/GLTFLoader.js';
 
 var c = document.getElementById("webgl");
 
